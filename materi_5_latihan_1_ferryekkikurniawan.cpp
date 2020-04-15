15b0c6083c486e590b218d9daa505e8b1e77dded
ferry-kurniawan <antitobatcbngk@gmail.com>
2020-04-16 02:35:00 +0700

    update

---
+++ materi_5_latihan_1_ferryekkikurniawan.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+void layang(int x, int y){
+	int hasil;
+
+	hasil = (x*y) / 2;
+	cout<<"Hasil : "<<hasil<<endl;
+}
+int main()
+{
+	int x,y;
+
+	cout<<"Menghitug Luas layang-layang\n";
+	cout<<"\nMasukkan diagonal satu\t :  ";
+	cin>>x;
+	cout<<"\nMasukkan diagonal dua\t : ";
+	cin>>y;
+
+	layang (x,y);
+
+	system("pause");
+	return 0;
+
+}
+
+
+
---
+++ materi_5_latihan_2_ferryekkikurniawan.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int layang(int x, int y){
+	int hasil;
+
+	hasil = (x*y) / 2;
+	cout<<"Hasil : "<<hasil<<endl;
+	return hasil;
+}
+int main()
+{
+	int x,y,hasil,templ;
+
+	cout<<"Menghitug Luas layang-layang\n";
+	cout<<"\nMasukkan diagonal satu\t :  ";
+	cin>>x;
+	cout<<"\nMasukkan diagonal dua\t : ";
+	cin>>y;
+
+	hasil = layang(x,y);
+	cout<<"hasil : "<<hasil<<endl;
+
+	templ = hasil<2?10:0;
+	cout<<templ<<endl;
+
+	cout<<"hasil : "<<hasil<<endl;
+
+
+	system("pause");
+	return 0;
+
+}
+
+
+
